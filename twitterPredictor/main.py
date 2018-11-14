eb8778111eab321c8843fd62a81cc3da86fa599b
“Hugo <hugo.sonnery@student-cs.fr>
2018-11-14 15:16:06 +0100

    Ajout de la première version de la fonctionnalité 3

---
+++ twitterPredictor/main.py
@@ -0,0 +1,11 @@
+# Twitter Predictor
+
+import twitterCollect as tc
+import twitterStorage as ts
+import twitterAnalysis as ta
+import twitterProcessing as tp
+
+tc.execute()
+ts.execute()
+ta.execute()
+tp.execute()
--- twitterPredictor/twitterCollect/main.py
+++ twitterPredictor/twitterCollect/main.py
@@ -3,7 +3,7 @@ import tweepy
 from credentials import *
 from tweepy.streaming import StreamListener
 
-
+count = 100
 
 def twitter_setup():
     # Cette fonction permet d'établir un lien avec l'API Tweeter. Elle renvoie un objet de type API si la connexion est un succès, None sinon 
@@ -59,3 +59,50 @@ def collect_by_streaming(terms_to_track):
     stream=tweepy.Stream(auth = connexion.auth, listener=listener) # Connexion au stream
     stream.filter(track = terms_to_track)
 
+
+
+def get_candidate_queries(num_candidate=1, file_path="../../inputData/"):
+    # Fonction permettant d'obtenir une liste de requêtes à partir de mots-clés et de hashtags concernant un candidat politique
+
+    keywordsPath = file_path + "keywords" + str(num_candidate) + ".txt"
+    hashtagsPath = file_path + "hashtags" + str(num_candidate) + ".txt"
+
+    keywordsFile = open(keywordsPath, "r")
+    hashtagsFile = open(hashtagsPath, "r")
+    
+    keywords = keywordsFile.readlines() # On obtient une liste de mots - clés (et des hashtags)
+    hashtags = hashtagsFile.readlines()
+
+    for k in keywords : # On nettoie les données au préalable
+	k.trim()
+	k.toLowerCase()
+
+    for h in hashtags:
+	h.trim()
+	h.toLowerCase()
+
+    keywordsFile.close()
+    hashtagsFile.close()   
+    
+    try: # on concatène les deux listes de requêtes
+        queries = keywords + hashtags
+	return queries
+    except IOError:
+       return []
+
+def get_tweets_from_candidates_search_queries(queries, twitter_api):
+    # Fonction permettant de concaténer les tweets renvoyés pour chaque requête sur l'API twitter fournie en argument 
+    result = []
+	
+    for query in queries : # Pour chaque requête, on ajoute au résultat final le résultat de l'appel à l'API
+        result += twitter_api.search(q = query, rpp = count)
+	
+        return result
+
+def get_replies_to_candidate(num_candidate, idTweet):
+    # Fonction retournant pour l'id du tweet passé en paramètre l'ensemlbe de ses réponses
+
+    
+    
+def get_retweets_of_candidate(num_candidate, idTweet):
+    # Fonction retournant pour l'id du tweet passé en paramètre l'ensemble de ses retweets
