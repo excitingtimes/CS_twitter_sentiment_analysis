3a03a38e47bc6d35efc94d0b7d354622c2dff4b8
valencourlouis <louis.valencour@student-cs.fr>
2018-11-15 11:24:55 +0100

    actualisation du main

--- twitterPredictor/twitterProcessing/main.py
+++ twitterPredictor/twitterProcessing/main.py
@@ -130,3 +130,31 @@ def execute(tweets):
         avg_RT.append(avg_RT(candidats[-1]))
 
     return {"avg_hashtag" : avg_hashtag, "max_hashtag" : max_hashtag, "min_hashtag" : min_hashtag, "min_len_tweets" : min_len_tweets, "max_len_tweets" : max_len_tweets,"avg_len_tweets" : avg_len_tweets, "min_fav" : min_fav, "max_fav" : max_fav, "avg_fav" : avg_fav, "min_RT" : min_RT, "max_RT" : max_RT, "avg_RT" : avg_RT} 
+
+from textblob import TextBlob
+from textblob import word
+
+#on suppose que tweet est la chaine de caractère contennant l'ensemble des tweets
+
+
+tweets = TextBlob(tweet)
+
+
+def tweets_lematises(tweets):
+    """renvoie la liste des mots lémmatisés pour l'ensemble des tweets"""
+    mots_lematises=[]
+    for words in tweets:
+        w=word(words)
+        lemma=w.lemmatize()
+        mots_lematises;append(lemma)
+    return(mots_lematises
+    
+def tweets_mots_uniques(tweets):
+    """renvoie la liste des mots uniques pour l'ensemble des tweets"""
+    mots_unique=[]
+    for words in tweets:
+        if tweet.word_counts[words]==1:
+            mots_unique.append(words)
+    return(mots_unique)
+    
+        
